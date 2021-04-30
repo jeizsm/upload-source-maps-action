61655991a6b4a635083ea743a33955b719b765d3
Marat Safin <jeizsm@gmail.com>
2021-04-30 12:18:00 +0300

    take upload as input

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 # `$*` expands the `args` supplied in an `array` individually
 # or splits `args` in a string separated by whitespace.
-sh -c "bugsnag-source-maps upload-browser --api-key $1 --app-version $2 --base-url $3 --directory $4"
+sh -c "bugsnag-source-maps $1 --api-key $2 --app-version $3 --base-url $4 --directory $5"
