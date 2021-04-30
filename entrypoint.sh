cff878228a13e24c7e234293332d37a47703db7c
Marat Safin <jeizsm@gmail.com>
2021-04-30 14:43:05 +0300

    add --overwrite option

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,8 @@
 # or splits `args` in a string separated by whitespace.
 if [ $1 == "upload-browser" ]
 then
-  sh -c "bugsnag-source-maps $1 --api-key $2 --app-version $3 --base-url $4 --directory $5"
+  sh -c "bugsnag-source-maps $1 --api-key $2 --app-version $3 --base-url $4 --directory $5 --overwrite"
 elif [ $1 == "upload-node" ]
 then
-  sh -c "bugsnag-source-maps $1 --api-key $2 --app-version $3 --directory $5"
+  sh -c "bugsnag-source-maps $1 --api-key $2 --app-version $3 --directory $5 --overwrite"
 fi
