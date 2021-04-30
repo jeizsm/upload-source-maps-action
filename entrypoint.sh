43d2085325154e0212f8d5a9a8add23598dbe128
Marat Safin <jeizsm@gmail.com>
2021-04-30 13:21:12 +0300

    fix upload-node

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,10 @@
 
 # `$*` expands the `args` supplied in an `array` individually
 # or splits `args` in a string separated by whitespace.
-sh -c "bugsnag-source-maps $1 --api-key $2 --app-version $3 --base-url $4 --directory $5"
+if [ $1 == "upload-browser" ]
+then
+  sh -c "bugsnag-source-maps $1 --api-key $2 --app-version $3 --base-url $4 --directory $5"
+elif [ $1 == "upload-node" ]
+then
+  sh -c "bugsnag-source-maps $1 --api-key $2 --app-version $3 --directory $5"
+fi
